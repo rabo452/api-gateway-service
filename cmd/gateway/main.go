e14623eeb721677144aab3de3f3618cbd559e40a
Dmitro <10252199@stud.op.edu.ua>
2024-12-06 00:56:23 +0200

    feat: added docker compose to the solution. Added the "air" hot reloading tool.

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	APP_PORT := os.Getenv("APP_PORT")
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":"+APP_PORT, r)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,11 @@
-module gateway-service
+module github.com/rabo452/api-gateway-service
 
 go 1.23.4
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	golang.org/x/time v0.8.0 // indirect
+)
